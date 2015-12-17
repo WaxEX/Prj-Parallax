b022bf2da781e4d79071b218bd3c7d80adb033e7
Wataru.Mizutani <rollthesh@gmail.com>
2015-12-18 05:30:12 +0900

    遠景シーンも入れてみた

--- Unity/WalkingDemo/Assets/SimpleMove.cs
+++ Unity/WalkingDemo/Assets/SimpleMove.cs
@@ -4,24 +4,63 @@ using System.Collections;
 public class SimpleMove : MonoBehaviour {
     private Animator animator;
     private int isWalkId;
+    private int isCrashId;
+    public float RATIO = 1.0f;
+
+    private bool isFront = false;
+    private int CrashTimer = 0;
 
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
         isWalkId = Animator.StringToHash("isWalk");
+        isCrashId = Animator.StringToHash("isCrash");
     }
 	
 	// Update is called once per frame
 	void Update () {
-		float z = 1.0f*Input.GetAxis("Vertical");
+
+        if(CrashTimer > 0){
+            CrashTimer--;
+            return;
+        }
+
+        if (CrashTimer == 0){
+            animator.SetBool(isCrashId, false);
+        }
+
+        float z = RATIO * Input.GetAxis("Vertical");
 
         if (z != 0){
             transform.Translate(0, 0, z);
-            animator.SetBool(isWalkId, true);
+
+            if (!isFront && transform.position.z > 1.0f) {
+                CrashTimer = 15;
+                animator.SetBool(isCrashId, true);
+            }
+            else{
+                animator.SetBool(isWalkId, true);
+            }
+
+            // この辺ハードコーディング…
+            isFront = transform.position.z > 1.0f;
         }
-        else
-        {
+        else{
             animator.SetBool(isWalkId, false);
         }
 	}
+    
+    // シーン切替もここでやっちゃう
+    void FixedUpdate(){
+
+        if (Input.GetButton("Jump")){
+
+            if(Application.loadedLevelName == "Main"){
+                Application.LoadLevel("FarScene");
+            }
+            else{
+                Application.LoadLevel("Main");
+            }
+        }
+    }
 }
