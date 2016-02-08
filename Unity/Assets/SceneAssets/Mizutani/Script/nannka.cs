3fac95a9b0a8b3027b7de0e00bae8b94b332f9b1
Wataru Mizutani <mizutani@success-corp.co.jp>
2016-02-08 19:59:24 +0900

    開発用にファオルダを整理

---
+++ Unity/Assets/SceneAssets/Mizutani/Script/nannka.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nannka : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, 0, Random.Range(-1000, 1000));
+	
+	}
+}
