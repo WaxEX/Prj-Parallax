4edecffe5989c4730a7d70f5bf2ab4c76d1e6411
OhshiroSyoko <honey7395@gmail.com>
2016-05-10 13:51:12 +0900

    evelop

---
+++ Unity/Assets/SceneAssets/Ohshiro/Script/PlayerRazer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;using System.Collections;public class PlayerRazer : MonoBehaviour {    // プレイヤーから射出されるレーザ    //public GameObject laser;	// Use this for initialization	void Start () {		}		// Update is called once per frame	void Update () {        // マウスカーソル位置からカメラ中心点へ向かう光線(Rayを飛ばす)        Vector3 pos = new Vector3(Screen.width / 2, Screen.height / 2, 0);        Ray ray = Camera.main.ScreenPointToRay(pos);        // 光線と物体の衝突判定        RaycastHit rayHit;        if (Physics.Raycast(ray, out rayHit, Mathf.Infinity))        {
+//            Debug.Log(rayHit.collider.gameObject.name);
+            // 敵に光線が衝突している場合　敵に死刑宣告
+            if (rayHit.collider.gameObject.CompareTag("Enemy"))
+            {
+                rayHit.collider.gameObject.SendMessage("EnemyDelete");
+            }
+        }    }}
--- Unity/Assets/SceneAssets/Stage/Script/Enemy.cs
+++ Unity/Assets/SceneAssets/Stage/Script/Enemy.cs
@@ -1,20 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
+    //変数宣言
+    Vector3 position;
+    float speed;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        //変数初期化
+        speed = 0.1f;
+    }
 
-	void OnCollisionEnter(Collision collision) {
-		Destroy(gameObject);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        //enemy進行方向　マイナスＺ軸方向。等速。
+        transform.position += new Vector3(0, 0, -speed);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+    // Enemy delete
+    // 他のスクリプトから呼ばれる予定なのでpublic
+    public void EnemyDelete()
+    {
+        Destroy(gameObject);
+    }
 
 }
